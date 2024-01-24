6183d8ab0e128237613784659a6c1877677c979f
Alfinhi Hajid Dhia <alfinhihd@gmail.com>
2024-01-24 11:27:02 +0700

    add day 4

---
+++ Day 4/4B/main.js
@@ -0,0 +1,21 @@
+let favoriteBook1 = "Dune";
+let favoriteBook2 = "The Hitchhiker's Guide to the Galaxy";
+
+favoriteBook1 = "Twilight";
+
+
+const CONSTANT_BOOK = "1984";
+// CONSTANT_BOOK = "Animal Farm";
+
+let concatenatedBooks = favoriteBook1 + " and " + favoriteBook2;
+
+let pageNumber = 300; 
+let isAvailable = true; 
+let authorDetails = { name: "Frank Herbert", nationality: "American" }; 
+
+console.log("Updated favorite book 1:", favoriteBook1);
+console.log("Constant book:", CONSTANT_BOOK);
+console.log("Concatenated books:", concatenatedBooks);
+console.log("Number of pages:", pageNumber);
+console.log("Is the book available?", isAvailable);
+console.log("Author details:", authorDetails);
