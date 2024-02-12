2a9db5e48b1c1427656295e5be3f58a9723090be
Alfinhi Hajid Dhia <alfinhihd@gmail.com>
2024-02-12 14:00:23 +0700

    add changes

--- js_day_6/logic.js
+++ js_day_6/logic.js
@@ -1,31 +1,28 @@
 function majorityElement(nums) {
-    const check = {}; // Membuat objek untuk menyimpan jumlah kemunculan setiap nilai
+    const check = {}; 
     for (let i = 0; i < nums.length; i++) {
-      const value = nums[i]; // Mengambil nilai pada indeks ke-i
+      const value = nums[i]; 
       if (check[value]) {
-        // Memeriksa apakah nilai sudah ada dalam objek check
-        check[value] += 1; // Menambah jumlah kemunculan nilai
+        check[value] += 1; 
       } else {
-        check[value] = 1; // Menginisialisasi jumlah kemunculan nilai
+        check[value] = 1; 
       }
-      console.log(`Iterasi ${i + 1}:`, check); // Menampilkan objek check setiap iterasi
+    //   console.log(`Iterasi ${i + 1}:`, check); 
     }
   
-    var major = 0; // Variabel untuk menyimpan nilai mayoritas
+    var major = 0; 
     for (const [key, _] of Object.entries(check)) {
-      // Iterasi melalui setiap entri dalam objek check
+      
       if (major == 0) {
-        major = key; // Inisialisasi nilai mayoritas dengan nilai pertama dalam objek check
+        major = key; 
       }
       if (check[key] > check[major]) {
-        major = key; // Memperbarui nilai mayoritas jika jumlah kemunculan nilai baru lebih besar
+        major = key; 
       }
-      console.log(`Kunci: ${key}, Jumlah Kemunculan: ${check[key]}, Nilai Mayoritas Sementara: ${major}`); // Menampilkan proses iterasi
+    //   console.log(`Kunci: ${key}, Jumlah Kemunculan: ${check[key]}, Nilai Mayoritas Sementara: ${major}`); 
     }
   
-    console.log('Nilai Mayoritas Akhir:', major); // Menampilkan nilai mayoritas akhir
-  
-    return major; // Mengembalikan nilai mayoritas
+    return major; 
   }
   
   console.log(majorityElement([3, 2, 3])); // Output: 3 
