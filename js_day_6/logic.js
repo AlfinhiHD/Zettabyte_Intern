177ea8615d7a084289130be4b549a3d708cba7e4
Alfinhi Hajid Dhia <alfinhihd@gmail.com>
2024-02-12 14:50:36 +0700

    add new logic test

--- js_day_6/logic.js
+++ js_day_6/logic.js
@@ -1,34 +1,31 @@
-function majorityElement(nums) {
-    const check = {}; 
-    for (let i = 0; i < nums.length; i++) {
-      const value = nums[i]; 
-      if (check[value]) {
-        check[value] += 1; 
-      } else {
-        check[value] = 1; 
-      }
-    //   console.log(`Iterasi ${i + 1}:`, check); 
+var majorityElement = function(nums) {
+    var map = {};
+    var max = 0;
+    var majorNum = 0;
+
+    for (var i = 0; i < nums.length; i++) {
+        if (!map[nums[i]]) {
+            map[nums[i]] = 0; //inialisasi 0 jika blm ada
+            
+        }
+        map[nums[i]]++; //tambah nilai
+        
+        // console.log("\n")
+        // console.log(map);
+        // console.log("\n")
+
+        // jika kemunculan > max maka ganti majornum dan perbarui max
+        if (map[nums[i]] > max) {
+            // Simpan nilai yang baru ditemukan sebagai nilai mayoritas sementara
+            majorNum = nums[i];
+            // Simpan jumlah kemunculan nilai mayoritas sementara
+            max = map[nums[i]];
+        }
+        // console.log(`Iterasi ke-${i + 1}: ${nums[i]}, Jumlah Kemunculan: ${map[nums[i]]}, Mayoritas Sementara: ${majorNum}, Nilai Max Saat Ini: ${max}`);
     }
-  
-    var major = 0; 
-    for (const [key, _] of Object.entries(check)) {
-      
-      if (major == 0) {
-        major = key; 
-      }
-      if (check[key] > check[major]) {
-        major = key; 
-      }
-    //   console.log(`Kunci: ${key}, Jumlah Kemunculan: ${check[key]}, Nilai Mayoritas Sementara: ${major}`); 
-    }
-  
-    return major; 
-  }
-  
-  console.log(majorityElement([3, 2, 3])); // Output: 3 
-  console.log(majorityElement([3, 2, 3, 2])); // Output: 3 
-  console.log(majorityElement([3, 2, 3, 2, 3])); // Output: 3 
-  console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // Output: 2 
-  console.log(majorityElement([2, 2, 1, 2, 1, 2, 2, 3, 3, 1])); // Output: 2
-  
-  
+
+    return majorNum;
+};
+
+console.log(majorityElement([2, 2, 1, 2, 1, 2, 2, 3, 3, 3, 3, 1, 1])); //2
+console.log(majorityElement([2, 2, 1, 2, 1, 2, 0, 0, 3, 0, 0, 0, 0])); //0
