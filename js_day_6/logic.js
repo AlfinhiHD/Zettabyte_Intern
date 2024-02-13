52d137bf1f3a8f7eca209fc0a09d3f03aa201f11
Alfinhi Hajid Dhia <alfinhihd@gmail.com>
2024-02-13 08:19:46 +0700

    change var to let variable

--- js_day_6/logic.js
+++ js_day_6/logic.js
@@ -1,9 +1,9 @@
-var majorityElement = function(nums) {
-    var map = {};
-    var max = 0;
-    var majorNum = 0;
+let majorityElement = function(nums) {
+    let map = {};
+    let max = 0;
+    let majorNum = 0;
 
-    for (var i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         if (!map[nums[i]]) {
             map[nums[i]] = 0; //inialisasi 0 jika blm ada
             
@@ -27,7 +27,7 @@ var majorityElement = function(nums) {
     return majorNum;
 };
 
+console.log(majorityElement([3, 2, 3])); // Output: 3 
 console.log(majorityElement([2, 2, 1, 2, 1, 2, 2, 3, 3, 3, 3, 1, 1])); //2
 console.log(majorityElement([2, 2, 1, 2, 1, 2, 0, 0, 3, 0, 0, 0, 0])); //0
-console.log(majorityElement([3, 2, 3])); // Output: 3 
 console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // Output: 2 
