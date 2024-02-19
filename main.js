18570c240c31aa03f3685528443e9ef54dd0a338
Alfinhi Hajid Dhia <alfinhihd@gmail.com>
2024-02-19 13:30:20 +0700

    add changes

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-// // First function
-// function combineArray(arr: (number | string)[]): string {
-//   return arr.join(' ');
-// }
-// // Case 1
-// console.log(combineArray([1, 'data', '3', 'result']));
-// // Case 2
-// console.log(combineArray(['Bejo', 'has', '4', 'sport', 'car']));
-// Second function
+// FUNGSI KE-1
+function combineArray(arr) {
+    return arr.join(' ');
+}
+console.log("\nOutput fungsi ke-1");
+// Case 1
+console.log(combineArray([1, 'data', '3', 'result']));
+// Case 2
+console.log(combineArray(['Bejo', 'has', '4', 'sport', 'car']));
+// FUNGSI KE-2
 function sumOrConcat(arr) {
     if (arr.every(function (item) { return typeof item === 'number'; })) {
-        // return arr.reduce((acc, curr) => acc + curr, 0);
-        var sum_1 = 0;
-        arr.forEach(function (el) { return (sum_1 += el); });
-        console.log(sum_1);
-        return sum_1;
+        return arr.reduce(function (acc, curr) { return +acc + +curr; });
+        // let sum: number = 0;
+        // arr.forEach((el: number) => (sum += el));
+        // return sum;
+        // let sum: number = 0;
+        // for (let i = 0; i < arr.length; i++) {
+        //   sum += Number(arr[i]);
+        // }
+        // return sum;
     }
     else if (arr.every(function (item) { return typeof item === 'string'; })) {
         return arr.join(' ');
@@ -22,50 +27,39 @@ function sumOrConcat(arr) {
         return 'invalid input';
     }
 }
+console.log("\nOutput fungsi ke-2");
 // Case 1
 console.log(sumOrConcat([1, 2, 3, 4])); // Output: 10
 // Case 2
 console.log(sumOrConcat(['the', 'dolphins', 'of', 'zettacamp'])); // Output: 'the dolphins of zettacamp'
 // Case 3
 console.log(sumOrConcat(['Bejo', 'has', 4, 'sport', 'car'])); // Output: 'invalid input'
-// // Third function
-// type Product = {
-//   name: string;
-//   vendor: string;
-//   quantity: number;
-//   inStock: number;
-//   price: number;
-// };
-// type SimplifiedProduct = {
-//   name: string;
-//   buyable: boolean;
-//   totalPrice?: number;
-// };
-// function transformProduct(product: Product): SimplifiedProduct {
-//   const simplifiedProduct: SimplifiedProduct = {
-//     name: product.name,
-//     buyable: product.inStock > 0,
-//   };
-//   if (simplifiedProduct.buyable) {
-//     simplifiedProduct.totalPrice = product.quantity * product.price;
-//   }
-//   return simplifiedProduct;
-// }
-// // Case 1
-// const product1: Product = {
-//   name: 'Coca Cola',
-//   vendor: 'Coca Cola Company',
-//   quantity: 5,
-//   inStock: 100,
-//   price: 5000,
-// };
-// console.log(transformProduct(product1));
-// // Case 2
-// const product2: Product = {
-//   name: 'Sari Roti',
-//   vendor: 'Toko Kue Sebelah',
-//   quantity: 2,
-//   inStock: 0,
-//   price: 10000,
-// };
-// console.log(transformProduct(product2));
+function transformProduct(product) {
+    var simplifiedProduct = {
+        name: product.name,
+        buyable: product.inStock > 0,
+    };
+    if (simplifiedProduct.buyable) {
+        simplifiedProduct.totalPrice = product.quantity * product.price;
+    }
+    return simplifiedProduct;
+}
+console.log("\nOutput fungsi ke-3");
+// Case 1
+var product1 = {
+    name: 'Coca Cola',
+    vendor: 'Coca Cola Company',
+    quantity: 5,
+    inStock: 100,
+    price: 5000,
+};
+console.log(transformProduct(product1));
+// Case 2
+var product2 = {
+    name: 'Sari Roti',
+    vendor: 'Toko Kue Sebelah',
+    quantity: 2,
+    inStock: 0,
+    price: 10000,
+};
+console.log(transformProduct(product2));
--- main.ts
+++ main.ts
@@ -1,22 +1,33 @@
-// // FUNGSI KE-1
-// function combineArray(arr: (number | string)[]): string {
-//   return arr.join(' ');
-// }
+type stringNumArrtype = (string | number)[];
+type stringNumtype = string | number
 
-// // Case 1
-// console.log(combineArray([1, 'data', '3', 'result']));
+// FUNGSI KE-1
+function combineArray(arr: stringNumArrtype): string {
+  return arr.join(' ');
+}
 
-// // Case 2
-// console.log(combineArray(['Bejo', 'has', '4', 'sport', 'car']));
+console.log("\nOutput fungsi ke-1")
+// Case 1
+console.log(combineArray([1, 'data', '3', 'result'])); // Expect : ‘1 data 3 result’
+// Case 2
+console.log(combineArray(['Bejo', 'has', '4', 'sport', 'car'])); // Expect : ‘Bejo has 4 sport car’
 
 // FUNGSI KE-2
-function sumOrConcat(arr: any[]): string | number {
+function sumOrConcat(arr: stringNumArrtype): stringNumtype {
   if (arr.every((item) => typeof item === 'number')) {
-    // return arr.reduce((acc, curr) => acc + curr, 0);
+   
+    return arr.reduce((acc, curr) => +acc + +curr, 0);
 
-    let sum: number = 0;
-    arr.forEach((el: number) => (sum += el));
-    return sum;
+    // let sum: number = 0;
+    // arr.forEach((el: number) => (sum += el));
+    // return sum;
+
+    // let sum: number = 0;
+    // for (let i = 0; i < arr.length; i++) {
+    //   sum += Number(arr[i]);
+    // }
+    // return sum;
+
   } else if (arr.every((item) => typeof item === 'string')) {
     return arr.join(' ');
   } else {
@@ -24,59 +35,59 @@ function sumOrConcat(arr: any[]): string | number {
   }
 }
 
+console.log("\nOutput fungsi ke-2")
 // Case 1
 console.log(sumOrConcat([1, 2, 3, 4])); // Output: 10
-
 // Case 2
 console.log(sumOrConcat(['the', 'dolphins', 'of', 'zettacamp'])); // Output: 'the dolphins of zettacamp'
-
 // Case 3
 console.log(sumOrConcat(['Bejo', 'has', 4, 'sport', 'car'])); // Output: 'invalid input'
 
-// // FUNGSI KE-3
-// type Product = {
-//   name: string;
-//   vendor: string;
-//   quantity: number;
-//   inStock: number;
-//   price: number;
-// };
+// FUNGSI KE-3
+type Product = {
+  name: string;
+  vendor: string;
+  quantity: number;
+  inStock: number;
+  price: number;
+};
 
-// type SimplifiedProduct = {
-//   name: string;
-//   buyable: boolean;
-//   totalPrice?: number;
-// };
+type SimplifiedProduct = {
+  name: string;
+  buyable: boolean;
+  totalPrice?: number;
+};
 
-// function transformProduct(product: Product): SimplifiedProduct {
-//   const simplifiedProduct: SimplifiedProduct = {
-//     name: product.name,
-//     buyable: product.inStock > 0,
-//   };
+  function transformProduct(product: Product): SimplifiedProduct {
+    const simplifiedProduct: SimplifiedProduct = {
+      name: product.name,
+      buyable: product.inStock > 0,
+    };
 
-//   if (simplifiedProduct.buyable) {
-//     simplifiedProduct.totalPrice = product.quantity * product.price;
-//   }
+    if (simplifiedProduct.buyable) {
+      simplifiedProduct.totalPrice = product.quantity * product.price;
+    }
 
-//   return simplifiedProduct;
-// }
+    return simplifiedProduct;
+  }
 
-// // Case 1
-// const product1: Product = {
-//   name: 'Coca Cola',
-//   vendor: 'Coca Cola Company',
-//   quantity: 5,
-//   inStock: 100,
-//   price: 5000,
-// };
-// console.log(transformProduct(product1));
+console.log("\nOutput fungsi ke-3")
+// Case 1
+const product1: Product = {
+  name: 'Coca Cola',
+  vendor: 'Coca Cola Company',
+  quantity: 5,
+  inStock: 100,
+  price: 5000,
+};
+console.log(transformProduct(product1));
 
-// // Case 2
-// const product2: Product = {
-//   name: 'Sari Roti',
-//   vendor: 'Toko Kue Sebelah',
-//   quantity: 2,
-//   inStock: 0,
-//   price: 10000,
-// };
-// console.log(transformProduct(product2));
+// Case 2
+const product2: Product = {
+  name: 'Sari Roti',
+  vendor: 'Toko Kue Sebelah',
+  quantity: 2,
+  inStock: 0,
+  price: 10000,
+};
+console.log(transformProduct(product2));
