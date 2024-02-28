9ad0f8ed95a60d274285a71e5f626fb4dd698f38
Alfinhi Hajid Dhia <alfinhihd@gmail.com>
2024-02-28 14:37:53 +0700

    add delete

--- angular-project/src/app/selectedplant/plant-detail/plant-detail.component.ts
+++ angular-project/src/app/selectedplant/plant-detail/plant-detail.component.ts
@@ -1,17 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { PlantType } from 'src/app/shared/helpers/interface';
+import { PlantService } from 'src/app/shared/service/plant.service';
 
 @Component({
   selector: 'app-plant-detail',
   templateUrl: './plant-detail.component.html',
-  styleUrls: ['./plant-detail.component.scss']
+  styleUrls: ['./plant-detail.component.scss'],
 })
 export class PlantDetailComponent implements OnInit {
-  @Input() plant : PlantType
+  @Input() plant: PlantType;
 
-  constructor() { }
+  constructor(private plantService: PlantService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onDeletePlant(id: string): void {
+    this.plantService.deleteplant(id);
+    this.router.navigate(['/home']);
   }
-
 }
