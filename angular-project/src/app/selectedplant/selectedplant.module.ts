3419858501937679f9ecbccfeeae8aaae36d5ed7
Alfinhi Hajid Dhia <alfinhihd@gmail.com>
2024-02-28 14:09:14 +0700

    add changes

--- angular-project/src/app/selectedplant/selectedplant.module.ts
+++ angular-project/src/app/selectedplant/selectedplant.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectedPlantComponent } from './selectedplant.component';
-import { PlantEditComponent } from './plant-edit/plant-edit.component';
 import { PlantDetailComponent } from './plant-detail/plant-detail.component';
 import { AppRoutingModule } from '../app-routing.module';
 
@@ -10,7 +9,6 @@ import { AppRoutingModule } from '../app-routing.module';
 @NgModule({
   declarations: [
     SelectedPlantComponent,
-    PlantEditComponent,
     PlantDetailComponent
   ],
   imports: [
@@ -18,7 +16,6 @@ import { AppRoutingModule } from '../app-routing.module';
     AppRoutingModule
   ],
   exports: [
-    PlantEditComponent,
     PlantDetailComponent,
     SelectedPlantComponent
   ]
