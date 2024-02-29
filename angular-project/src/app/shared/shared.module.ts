3f631cfa8ce59014ef922a8dd685364df8dcf093
Alfinhi Hajid Dhia <alfinhihd@gmail.com>
2024-02-29 09:15:01 +0700

    add changes

--- angular-project/src/app/shared/shared.module.ts
+++ angular-project/src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    NotfoundComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    NotfoundComponent
   ]
 })
 export class SharedModule { }
