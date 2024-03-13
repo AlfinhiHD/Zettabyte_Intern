3bf2b7b30f0773a470ba1f2e2f63d1232363637c
Alfinhi Hajid Dhia <alfinhihd@gmail.com>
2024-03-13 10:01:19 +0700

    initialized files

---
+++ angular-project/src/app/school/school.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SchoolComponent } from './school.component';
+
+
+
+@NgModule({
+  declarations: [
+    SchoolComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SchoolComponent
+  ]
+})
+export class SchoolModule { }
