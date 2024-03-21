2f0090842d9b76639111f43563c7ea656c2bad1f
Alfinhi Hajid Dhia <alfinhihd@gmail.com>
2024-03-21 15:56:48 +0700

    add changes

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
+
+  isSidenavOpen: boolean = true;
+
+  toggleSidenav() {
+    this.isSidenavOpen = !this.isSidenavOpen;
+  }
 }
